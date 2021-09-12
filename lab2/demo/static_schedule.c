cb89c69d1e0547f7f88856ba2ca90a309204752a
florinrm <florinrmfm3@gmail.com>
2021-09-13 00:23:48 +0300

    Updated demo

--- lab2/demo/static_schedule.c
+++ lab2/demo/static_schedule.c
@@ -4,21 +4,28 @@
 #define NUM_THREADS 4
 
 int w(int n) {
-    int s = 0;
+    long s = 0;
     for (int i = 0; i < n; i++) {
-        s++;
+        for (int k = 0; k < 10000000; k++) {
+            s++;
+        }
     }
 }
 
 int main(int argc, char** argv) {
     int i;
+    double t1, t2;
     omp_set_num_threads(NUM_THREADS);
 
-    #pragma omp parallel for private(i) schedule(static, 4)
+    t1 = omp_get_wtime();
+    #pragma omp parallel for private(i) schedule(static, 1)
     for (i = 0; i < 16; i++) {
         w(i);
         printf("iteration no. %d | thread no. %d\n", i, omp_get_thread_num());
     }
+    t2 = omp_get_wtime();
+
+    printf("Total execution time = %lf\n", (t2 - t1));
     
     return 0;
 }
